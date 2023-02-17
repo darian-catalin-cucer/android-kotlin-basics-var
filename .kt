d5362c829f032e22648eee07524f1ce40d45d215
Darian-Cătălin Cucer <86965240+cucerdariancatalin@users.noreply.github.com>
2023-02-17 13:29:32 +0200

    Create .kt
    Explanation:
    
    The var keyword is used to declare a mutable variable that can be reassigned to a different value.
    In the first example, a var variable count is declared with the value 1. This variable can be reassigned to a different value, as shown in the next line.
    In the second example, count is reassigned to the value 2, and then printed to the console.
    In the third example, a var variable greeting is declared without an initial value. This variable can be assigned a value later, as shown in the next line.
    In the fourth example, a var variable pi is declared with an initial value of 3.14. This variable can also be reassigned to a different value later.

---
+++ .kt
@@ -0,0 +1,16 @@
+// Examples of var in Kotlin
+fun main() {
+    var count = 1
+    println("Count is $count")
+    
+    count = 2
+    println("Count is now $count")
+
+    var greeting: String
+    greeting = "Hello"
+    println("$greeting, world!")
+
+    var pi: Double = 3.14
+    pi = 3.14159
+    println("The value of pi is $pi")
+}
